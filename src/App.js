897b3f9a9f54070e9142be770c92cf23a053b64b
CatalinOSBI <garjoaba_c@yahoo.com>
2023-09-26 15:18:01 +0300

    Learning more

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import Header from './Header';
+import './Header.css';
 
 
 function App(){
     return(
+        <>
         <h1>Comicsc</h1>
+        <Header/>
+        </>
     );
 }
 
