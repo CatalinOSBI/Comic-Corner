7010b90f42d855485425d1e44fc71f5883f8f8a7
CatalinOSBI <garjoaba_c@yahoo.com>
2023-09-26 20:15:41 +0300

    header update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import Header from './Header';
-import './Header.css';
+
 
 
 function App(){
     return(
         <>
-        <h1>Comicsc</h1>
         <Header/>
         </>
     );
--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,20 @@
 import React from 'react'
+import logo from './cc-high-resolution-logo-color-on-transparent-background.png'
 
 export default function Header() {
   return (
-    <h2 className='batman'>Batman</h2>
+    <header>
+      <div className='logoContainer'>
+        <p>
+          Comic<br/>&emsp;&ensp;Corner
+        </p>
+        <img className='imgLogo' src={logo} alt='sitelogo'/>
+      </div>
+      <div className='linkContainer'>
+        <a className='line'>Latest Releases</a>
+        <a className='line'>Comic Spotlight</a>
+        <a className='line'>News</a>
+      </div>
+    </header>
   )
 }
