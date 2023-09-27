b98d6fad1571f21debd2f2917de9e9db01b36136
CatalinOSBI <garjoaba_c@yahoo.com>
2023-09-27 20:26:29 +0300

    added Latest Releases section

--- src/ContentOne.js
+++ src/ContentOne.js
@@ -3,7 +3,15 @@ import React from 'react'
 export default function () {
   return (
     <div className='contentOne'>
+      <div className='title'>
         <h1>Latest Releases</h1>
+      </div>
+      <div className='middle'>
+        <div className='comic'>a</div>
+        <div className='comic'>b</div>
+        <div className='comic'>c</div>
+        <div className='comic'></div>
+      </div> 
     </div>
   )
 }
