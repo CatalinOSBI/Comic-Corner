4df8f7a3559811859a56225ba5788548bc547154
CatalinOSBI <garjoaba_c@yahoo.com>
2024-05-22 20:33:31 +0300

    Your Comics Update (Guts and glory)

--- src/Modal/YourComics.js
+++ src/Modal/YourComics.js
@@ -1,34 +1,68 @@
-import React from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import './Modal.css'
 
 const YourComics = () => {
+  const [comicFolders, setComicFolders] = useState(JSON.parse(localStorage.getItem('Comic Folders')));
+  const folderNameRef = useRef()
 
-  const test = () => {
+  //Update LocalStorage whenever the state changes
+  useEffect(() => {
 
-    let Folder = {
+    localStorage.setItem('Comic Folders', JSON.stringify(comicFolders))
 
+  }, [comicFolders]);
 
-      FieldA: 'A',
-      FieldB: 'B',
-      FieldC: 'C',
+  //Mapping for modalFolders
+  const modalFolders = comicFolders.ComicFolders.map((item, index) => (
+    <div key={index} className='modalFolder'>
+      <p>{index}</p>
+    </div>
+  ));
 
+  //Update
+  const handleUpdate = () => {
+    let folderName = folderNameRef.current.value;
+  
+    setComicFolders((prev) => ({
+      ...prev,
+      ComicFolders: [
+        ...prev.ComicFolders,
+        {
+          [folderName]: [
+            {
+              image: 'A',
+              title: 'A',
+              author: 'A',              //GUTS AND GLORY
+              description: 'A',
+            },
+          ],
+        },
+      ],
+    }));
+  };
 
+  //Update
+  const handleStorageUpdate = () => {
 
+    const update = {
+      ComicFolders:[]
     }
 
-    localStorage.setItem('Comic Folder', JSON.stringify(Folder))
-
+    localStorage.setItem('Comic Folders', JSON.stringify(update))
   }
 
   return (
     <>
-      <div className='modalFolder'>
-        <button onClick={test}>test</button>
+      {modalFolders}
+
+      <div className='modalFolder' style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column' }}>
+        <button onClick={handleUpdate}>Add Folder</button>
+        <button onClick={handleStorageUpdate}>Storage</button>
+        <button onClick={() => { console.log(comicFolders) }}>Log</button>
+        <input id='folderName' name='folderName' placeholder='Folder Name' type='text' ref={folderNameRef} />
       </div>
 
-      <div className='modalFolder'>
-        <button onClick={test}>test</button>
-      </div>
+
     </>
   )
 }
