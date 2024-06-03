d3d07a31d98338cbb162e6506411fdb4571224a0
CatalinOSBI <garjoaba_c@yahoo.com>
2024-06-03 23:37:20 +0300

    Your comics - rename, bugs

--- src/Modal/ModalContext.js
+++ src/Modal/ModalContext.js
@@ -14,11 +14,16 @@ export const ModalProvider = ({ children }) => {
   const [activeContent, setActiveContent] = useState([0]);
   const [activeFolderContent, setActiveFolderContent] = useState([]);
   const [activeComicContent, setActiveComicContent] = useState([]);
+  const [activeFolderId, setActiveFolderId] = useState();
+  const [showDotsMenu, setShowDotsMenu] = useState(false);
   const folderNameRef = useRef()
+  const hideTimeoutRef = useRef();
   const selectorFolderNameRef = useRef()
 
   const folderIcon = <svg className='folderIcon' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#74a3eb" d="M64 480H448c35.3 0 64-28.7 64-64V160c0-35.3-28.7-64-64-64H288c-10.1 0-19.6-4.7-25.6-12.8L243.2 57.6C231.1 41.5 212.1 32 192 32H64C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64z" /></svg>
-  const dotsIcon = <svg className='dotsIcon' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 512"><path fill="#74a3eb" d="M64 360a56 56 0 1 0 0 112 56 56 0 1 0 0-112zm0-160a56 56 0 1 0 0 112 56 56 0 1 0 0-112zM120 96A56 56 0 1 0 8 96a56 56 0 1 0 112 0z" /></svg>
+  const dotsIcon = <svg anch className='dotsIcon' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 512"><path fill="#74a3eb" d="M64 360a56 56 0 1 0 0 112 56 56 0 1 0 0-112zm0-160a56 56 0 1 0 0 112 56 56 0 1 0 0-112zM120 96A56 56 0 1 0 8 96a56 56 0 1 0 112 0z" /></svg>
+  const trashIcon = <svg className='trashIcon' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="#393c3f" d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z" /></svg>
+  const penIcon = <svg className='penIcon' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#393c3f" d="M362.7 19.3L314.3 67.7 444.3 197.7l48.4-48.4c25-25 25-65.5 0-90.5L453.3 19.3c-25-25-65.5-25-90.5 0zm-71 71L58.6 323.5c-10.4 10.4-18 23.3-22.2 37.4L1 481.2C-1.5 489.7 .8 498.8 7 505s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L421.7 220.3 291.7 90.3z" /></svg>
   /////////////////////////////////////////
   //              Side Menu              //
   /////////////////////////////////////////
@@ -55,6 +60,38 @@ export const ModalProvider = ({ children }) => {
 
   }, [comicFolders]);
 
+  //Show DotsMenu
+  const handleShowDotsMenu = (e, folderId) => {
+    e.stopPropagation()
+    clearTimeout(hideTimeoutRef.current);
+    setActiveFolderId(folderId)
+    setShowDotsMenu(true)
+
+    hideTimeoutRef.current = setTimeout(() => {
+      setShowDotsMenu(false)
+    }, 1000);
+  }
+
+  //Hide DotsMenu
+  const handleHideDotsMenu = (e) => {
+    e.stopPropagation()
+    clearTimeout(hideTimeoutRef.current)
+
+    hideTimeoutRef.current = setTimeout(() => {
+      setActiveFolderId(null)
+      setShowDotsMenu(false)
+    }, 1000);
+  }
+
+  // Keep the menu visible while hovering
+  const handleKeepMenuVisible = (e, folderId) => {
+    e.stopPropagation();
+    clearTimeout(hideTimeoutRef.current);
+
+    setActiveFolderId(folderId);
+    setShowDotsMenu(true);
+  };
+
   //Update (Add new Folder)
   const handleUpdate = () => {
     let folderName = folderNameRef.current.value;
@@ -79,6 +116,7 @@ export const ModalProvider = ({ children }) => {
             },
           ],
           id: prev.ComicFolders.length, //id of the folder
+          folderName: folderName //folder name
         },
       ],
     }));
@@ -147,6 +185,18 @@ export const ModalProvider = ({ children }) => {
     )
   }
 
+  //Rename Folder
+  const handleRenameFolder = (e, folderId, newName) => {
+    e.stopPropagation()
+    
+    setComicFolders((prev) => ({
+      ...prev,
+      ComicFolders: prev.ComicFolders.map(folder => 
+        folder.id === folderId ? { ...folder, folderName: newName } : folder
+      )
+      
+    }))};
+
   //Delete Folder
   const handleDeleteFolder = (folderToBeDeleted, e) => {
     e.stopPropagation()
@@ -188,15 +238,24 @@ export const ModalProvider = ({ children }) => {
       <div key={folderIndex} className='modalFolderWrapper' onClick={() => handleGoToActiveFolder(folderComics)}>
         <div className='modalFolder' >
           <div style={{ position: 'absolute', width: '180px', height: 'auto', textWrap: 'wrap', textAlign: 'center' }}>
-            <p className='folderName'>{folderName}</p>
+            <p className='folderName'>{folder.folderName}</p>
           </div>
 
           {folderIcon}
-          <div className='dotsWrapper'>
+          <div onClick={(e) => handleShowDotsMenu(e, folder.id)} className='dotsWrapper'>
             {dotsIcon}
-          </div>
 
-          <button onClick={(e) => handleDeleteFolder(folder, e)}>Delete Folder</button>
+            {showDotsMenu && activeFolderId === folder.id &&
+              //DotsMenu
+              <div className='dotsMenu' onMouseLeave={(e) => handleHideDotsMenu(e, folder)} onMouseEnter={(e) => handleKeepMenuVisible(e, folder.id)}>
+                <ul className='dotsMenuList' style={{ listStyleType: 'none' }}>
+                  <li onClick={(e) => handleRenameFolder(e, folder.id , "Cookie")}> {penIcon} Rename </li>
+                  <li onClick={(e) => handleDeleteFolder(folder, e)}> {trashIcon} Delete </li>
+                </ul>
+              </div>
+            }
+
+          </div>
         </div>
       </div>
     );
--- src/Modal/YourComics.js
+++ src/Modal/YourComics.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react'
+import React, { useState} from 'react'
 import './Modal.css'
 import { useModal } from './ModalContext';
 
@@ -10,12 +10,9 @@ const YourComics = () => {
   const folderIconAdd = <svg style={{opacity:dynamicOpacity, transition:'all .218s'}} onClick={() => handleShowAddName()} className='folderIconAdd' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#74a3eb" d="M512 416c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V96C0 60.7 28.7 32 64 32H192c20.1 0 39.1 9.5 51.2 25.6l19.2 25.6c6 8.1 15.5 12.8 25.6 12.8H448c35.3 0 64 28.7 64 64V416zM232 376c0 13.3 10.7 24 24 24s24-10.7 24-24V312h64c13.3 0 24-10.7 24-24s-10.7-24-24-24H280V200c0-13.3-10.7-24-24-24s-24 10.7-24 24v64H168c-13.3 0-24 10.7-24 24s10.7 24 24 24h64v64z" /></svg>
 
   const {
-    comicFolders,
     folderNameRef,
     handleUpdate,
     modalFolders,
-    menuContent,
-    setActiveContent,
   } = useModal()
 
   const handleHideAddName = () => {
@@ -55,8 +52,8 @@ const YourComics = () => {
 
           {showNameFolder ? (
             <>
-              <div style={{ paddingTop: '16px', display: 'flex', justifyContent: 'center', alignItems: 'cener', flexDirection: 'column', opacity: dynamicOpacity, transition:'all .218s'}}>
-                <input style={{ width: '98.2%' }} id='folderName' name='folderName' placeholder='Folder Name' type='text' ref={folderNameRef} />
+              <div style={{ paddingTop: '16px', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', opacity: dynamicOpacity, transition:'all .218s'}}>
+                <input style={{ width: '98.2%' }} autoComplete='off' id='folderName' name='folderName' placeholder='Folder Name' type='text' ref={folderNameRef} />
                 {/* <button onClick={handleStorageUpdate}>Storage</button> */}
 
                 <div className='folderButtonsContainer' >
@@ -66,7 +63,9 @@ const YourComics = () => {
               </div>
             </>
           ) : (
+
             folderIconAdd
+            
           )}
 
         </div>
