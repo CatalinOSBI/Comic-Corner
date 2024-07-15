934985c85d2d237d300fc8f87613ecab34aadc4d
CatalinOSBI <garjoaba_c@yahoo.com>
2024-07-15 18:27:54 +0300

    asd

--- src/Modal/YourComics.jsx
+++ src/Modal/YourComics.jsx
@@ -49,6 +49,7 @@ const YourComics = () => {
 
       <div className='modalFolderWrapper' >
         <div className='modalFolder' title='Add Folder' >
+          <button onClick={()=>console.log(import.meta.env.VITE_APP_1)}>sdsad</button>
 
           {showNameFolder ? (
             <>
